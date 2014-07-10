d53c3d918e43eaf5e74994fe1920c1891ac87e21
Daniel Piker <danielpiker@gmail.com>
2014-07-10 10:10:36 +0100

    Initial commit
    Not cleaned up yet

---
+++ MeshMachine/MeshMachineInfo.cs
@@ -0,0 +1,18 @@
+﻿using Grasshopper.Kernel;
+
+namespace MeshMachine
+{
+    public class MeshMachineInfo : GH_AssemblyInfo
+    {
+        public override string AssemblyName
+        {
+            get
+            {
+                return "MeshMachine";
+            }
+        }
+
+        //Override here any more methods you see fit.
+        //Start typing public override..., select a property and push Enter.
+    }
+}
